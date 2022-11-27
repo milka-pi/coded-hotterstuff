a727b91501f454d47b9df6f077bad3961183b76f
Ubuntu <ubuntu@ip-172-31-52-72.ec2.internal>
2022-11-27 15:14:08 +0000

    adding folder name to netscript

--- hotstuff-node/netscript.sh
+++ hotstuff-node/netscript.sh
@@ -19,9 +19,9 @@ msg=$(bash ../../nanonet/script.sh add 1 0 $bw)
 msgarray=($msg)
 ip=${msgarray[6]}
 ipAddresses="$ip"
+logFolder="tcp_coded_logs_$numNodes"
 
-bash delete_log_folder.sh
-mkdir -p logs
+mkdir -p $logFolder
 go build
 ## create rest of network nodes (with idx >= 1)
 for (( index=1; index<numNodes; index++ ))
@@ -48,7 +48,7 @@ echo "List of IP addresses: $ipAddresses"
 # 2nd Step: Run hotstuff-node for each node, passing the full list of IP addresses as an argument
 
 for (( index=0; index<numNodes; index++ )); do
-	ip netns exec ramjet-s1-n$index ./hotstuff-node -numNodes=$numNodes -index=$index -ipAddresses=$ipAddresses &> logs/hotstuff-$index.log &
-	ip netns exec ramjet-s1-n$index bmon -o format:fmt='$(element:name) rxbytes=$(attr:rx:bytes) txbytes=$(attr:tx:bytes)\n' -p 'veth0' &> logs/node-$index-traffic.log &
-	ip netns exec ramjet-s1-n$index ./tcpcollect.sh &> logs/node-$index-tcp.log &
+	ip netns exec ramjet-s1-n$index ./hotstuff-node -numNodes=$numNodes -index=$index -ipAddresses=$ipAddresses &> $logFolder/hotstuff-$index.log &
+	ip netns exec ramjet-s1-n$index bmon -o format:fmt='$(element:name) rxbytes=$(attr:rx:bytes) txbytes=$(attr:tx:bytes)\n' -p 'veth0' &> $logFolder/node-$index-traffic.log &
+	ip netns exec ramjet-s1-n$index ./tcpcollect.sh &> $logFolder/node-$index-tcp.log &
 done
