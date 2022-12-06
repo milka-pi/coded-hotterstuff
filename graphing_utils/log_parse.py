d73af3219e298a9be1e789f68a816459cfb55ff4
Milka Piszczek <milka@mit.edu>
2022-12-06 18:56:08 -0500

    adding comments to make log_parse more understandable adding the logs for the microbenchmark so it should be able to run out of the box

--- graphing_utils/log_parse.py
+++ graphing_utils/log_parse.py
@@ -18,14 +18,19 @@ parser.add_argument('--dir',
                     help="where to look for log folders",
                     default='logs_arch')
 
-# parser.add_argument('--node',
-#                     type=int,
-#                     help="Which specific node id's log to parse",
-#                     default=0)
-
 args = parser.parse_args()
 num_nodes = 4 
 to_timestamp = lambda x: datetime.strptime(x,"%Y-%m-%dT%H:%M:%S.%fZ").timestamp() 
+
+
+# General order would be:
+
+# Leader node sends the proposal - "send" log
+
+# Follower has a recieve message from the leader (shareNumber = 0) - "recieve" log
+# Follower forwards its part of the chunk - "forward" log
+# Follower has enough sub-chunks to decode - "ready" log
+# Follower decodes the sub-chunks into a proposal - "decode" log
 
 def process_file(num_nodes, n, data):
     f = open(f"{args.dir}/tcp_coded_logs_microbenchmark_{num_nodes}/hotstuff-{n}.log", "r")
@@ -45,6 +50,8 @@ def process_file(num_nodes, n, data):
             data.setdefault(block_hash, {})
             data[block_hash].setdefault(node, {})
             if action in ["send", "decode", "ready"]:
+                # have this if statement becuase I just keep the first time the action happens
+                # since i don't get why it might happen twice haha
                 if action not in data[block_hash][node]: 
                     data[block_hash][node][action] = timestamp
             elif action == "receive":
@@ -61,30 +68,49 @@ def process_file(num_nodes, n, data):
             continue
 
 if __name__ == "__main__":
-    # # first find first timestamp
-    # for line in full_file:
-    #     split_line = line.split()
-    #     if len(split_line) < 2 or split_line[1] != LOG_LEVEL:
-    #         continue
-    #     log_start = to_timestamp(split_line[0])
-    #     break
-    # then iterate over whole file
     data = {}
     for n in range(num_nodes):
         process_file(num_nodes, n, data)
+    
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(data)
     print(len(data))
     per_block_data = {}
+
+    #### AN ENTRY IN data DICT LOOKS LIKE THIS #####
+    # KEY = block hash
+    # dicts are actions that take place in the nodes & their time stamps
+    # 's0+dCVQ5w8iE2Z+DCswdFLdP8QD8T++9aicJwYKZbok=': {   0: {   'decode': 1670386397.465, # time node 0 decoded this block
+    #                                                        'forward': {   1: 1670386396.334, # time node 0 fwdd this block to node 1
+    #                                                                       3: 1670386396.334},  # time node 0 fwdd this block to node 3
+    #                                                        'ready': 1670386397.465,  # time node 0 was ready to decode this block
+    #                                                        'receive': {   0: 1670386396.333, # time node 0 got its chunk part from leader
+    #                                                                       1: 1670386397.464, # time node 0 got 1st chunk part from peer (not necessarily from node 1)
+    #                                                                       2: 1670386399.063}}, # time node 0 got 2nd chunk part from peer 
+    #                                                 1: {   'decode': 1670386397.25,
+    #                                                        'forward': {   0: 1670386396.511,
+    #                                                                       3: 1670386396.511},
+    #                                                        'ready': 1670386397.249,
+    #                                                        'receive': {   0: 1670386397.249,
+    #                                                                       1: 1670386396.511,
+    #                                                                       2: 1670386398.96}},
+    #                                                 2: {   'send': 1670386395.154}, # time this block was sent (2 must have been the leader)
+    #                                                 3: {   'decode': 1670386397.562,
+    #                                                        'forward': {   0: 1670386397.733,
+    #                                                                       1: 1670386397.733},
+    #                                                        'ready': 1670386397.562,
+    #                                                        'receive': {   0: 1670386397.325,
+    #                                                                       1: 1670386397.562,
+    #                                                                       2: 1670386397.733}}},
+
     for block_hash, node_info in data.items():
-        print("============")
+        # print("============")
         decodes = []
         forwards = []
         peer_receieve = []
         leader_receieve = []
         peer_ready = []
         send = 0
-        pp.pprint(node_info)
         per_block_data.setdefault(block_hash, {})
         for node, node_info in node_info.items():
             # print(node)
@@ -109,11 +135,19 @@ if __name__ == "__main__":
         if send == 0 or len(decodes) == 0:
             del per_block_data[block_hash]
             continue
-        per_block_data[block_hash]["last_decode"] = max(decodes) - per_block_data[block_hash]["send"] 
-        per_block_data[block_hash]["last_ready"] = max(peer_ready) - per_block_data[block_hash]["send"] 
+
+        compare_func = max # min for first node to do it, max for last node to do it
+        per_block_data[block_hash]["last_decode"] = compare_func(decodes) - per_block_data[block_hash]["send"] 
+        per_block_data[block_hash]["last_ready"] = compare_func(peer_ready) - per_block_data[block_hash]["send"] 
+        
+        # You can uncomment this to visualize the data better perhaps
         # print(f"block hash: {block_hash}")
         # print(f"send time: {send}")
         # print(f"last receive from leader: {max(leader_receieve)-send}")
         # print(f"last receive from peer: {max(peer_receieve)-send}")
         # print(f"last decode: {max(decodes)-send}")
-    pp.pprint(per_block_data)
+        
+    rates = []
+    for block_hash in per_block_data:
+        last = per_block_data[block_hash]["last_ready"]
+        rates.append(8/last)
